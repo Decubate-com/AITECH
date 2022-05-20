b7431248e10e695f1d17834a95d3f8bbe318dbb2
mj-dcb <jay@decubate.com>
2022-05-20 11:16:01 +0200

    replaced the public mint function with a constructor call

--- contracts/AITECH.sol
+++ contracts/AITECH.sol
@@ -9,29 +9,20 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract AITECH is ERC20, Ownable, AccessControl, ERC20Burnable {
 
-    // Create a new role identifier for the minter role
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
     // Create a new role identifier for the burner role
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     constructor() ERC20("AITECH", "AITECH") {
 
+        // Mint 8 billion tokens
+        _mint(msg.sender,8000000000*10**18);
+
         // Grant the contract deployer the default admin role: it will be able
         // to grant and revoke any roles
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
-        // Grant the minter role to a specified account
-        _setupRole(MINTER_ROLE, msg.sender);
-
         // Grant the burner role to a specific account
         _setupRole(BURNER_ROLE, msg.sender);
-    }
-
-    // This function uses the virtual _mint() function
-    // it can be called by the MINTER_ROLE
-    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
     }
 
     // This function overrides the burn() function from the ERC20Burnable extension

704a7dd4f4323a44b7a1bce558a200b4dd1e773e
Jay <81908549+mj-dcb@users.noreply.github.com>
2023-05-31 15:21:36 +0200

    Update AITECH.sol
    Fixed issues and improved score

--- src/AITECH.sol
+++ src/AITECH.sol
@@ -1,15 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.16;
+//** AITECH Token ERC20 Token for Mainnet */
+pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract AITECH is ERC20, Ownable, ERC20Burnable {
+contract AITECH is ERC20Burnable {
     constructor() ERC20("AITECH", "AITECH") {
-        // Mint 2 billion tokens
-        _mint(msg.sender, 2000000000 * 10 ** 18);
+        _mint(msg.sender, 2_000_000_000 * 10 ** decimals());
     }
 }
